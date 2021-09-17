b4a07e115dabb089555a2001fbf0b9501b86db7e
gyaninascimento <gyanipnascimento@gmail.com>
2021-09-17 17:44:09 -0300

    Create exerc26.js

---
+++ 8 - expressões de objeto/exerc26.js
@@ -0,0 +1,13 @@
+/*Crie um nome para você mesmo
+Neste desafio, você adicionará um par propriedade-valor a um objeto. Depois, use essa propriedade em uma função print().
+
+INSTRUÇÕES
+Adicione uma propriedade last dentro do objeto name e adicione uma string como o valor
+Atualize a função print() para imprimir o valor de name.last*/
+
+var name = {
+    first: 'Martin ',
+    middle: 'Luther ',
+    last: 'king',
+  };
+  print(name.first + name.middle + name.last);
