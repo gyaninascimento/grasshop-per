51ede343b55e81cce9c13f388ff6101f68d6f77f
gyaninascimento <gyanipnascimento@gmail.com>
2021-09-20 19:17:04 -0300

    exerc2

---
+++ Fundamentos II/exerc2.js
@@ -0,0 +1,9 @@
+/**A um dia
+Neste desafio, você usará o método .replace() para substituir parte de uma string. O método .replace() procura um valor e, se o encontrar, substituirá o 1º valor por uma nova string. Por exemplo, para mudar a string 'lemons' em'lemonade', você usaria .replace('lemons', 'lemonade').
+
+INSTRUÇÕES
+Altere o 2º argumento em .replace() de 'mato' para 'morrow' */
+
+var message = 'Today we are going shopping!';
+message = message.replace('day', 'morrow');
+print(message);
