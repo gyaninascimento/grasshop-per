fe01420c91f4511ad14ddf27bf2ca2de9de7ca47
gyaninascimento <gyanipnascimento@gmail.com>
2021-09-22 18:19:08 -0300

    exerc14

---
+++ Fundamentos II/exerc14.js
@@ -0,0 +1,9 @@
+/**Quantos?
+Neste desafio, você usará a propriedade .length para contar o número total de itens em uma array.
+
+INSTRUÇÕES
+Atualize o valor de numberOfDestinations para ser igual à propriedade .length de listOfDestinations */
+
+import { listOfDestinations } from 'grasshopper.travel';
+let numberOfDestinations = listOfDestinations.length;
+console.log('There are ' + numberOfDestinations + ' to choose from.');
