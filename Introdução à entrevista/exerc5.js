943965aa6fb865ac2710c754a866ace35e29b834
gyaninascimento <gyanipnascimento@gmail.com>
2021-09-30 13:40:16 -0300

    exerc5

---
+++ Introdução à entrevista/exerc5.js
@@ -0,0 +1,10 @@
+/**Saída interna
+Nesta lição, você criará uma função que pega uma array de números e retorna a soma de seus itens.
+
+INSTRUÇÕES
+Dentro do bloco de código de sum():
+
+Declare uma variável para usar como saída. Defina seu valor como 0
+Dentro de um laço for...of, adicione cada elemento à variável
+Abaixo de laço for...of, retorne à variável */
+
