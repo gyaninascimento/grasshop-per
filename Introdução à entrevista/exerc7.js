8abd28a183c3d531a9ebc3900656e4265969fd89
gyaninascimento <gyanipnascimento@gmail.com>
2021-09-30 16:25:04 -0300

    exerc7

---
+++ Introdução à entrevista/exerc7.js
@@ -0,0 +1,14 @@
+/**Números diferentes de zero
+Neste desafio, você registrará os números de -3 a 3, mas pulará 0.
+
+INSTRUÇÕES
+Altere o código dentro do laço for para que cada número seja registrado, exceto 0.
+
+Use uma if statement para verificar o valor de i
+Registre o número apenas se o valor não for 0 */
+
+for (let i = 0 - 3; i < 4; i++) {
+    if (i) {
+      console.log(i);
+    }
+  }
